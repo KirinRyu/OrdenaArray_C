5c580c2f584cc4824d9179203d07f74d01b50190
Ryu Satoy <dan_ryuichi@outlook.com>
2024-04-22 15:11:52 -0300

    modified to bubbleSort

--- ordenaArray.c
+++ ordenaArray.c
@@ -1,36 +1,27 @@
 #include <stdio.h>
 
-int buscaMenor(int *vetor);
-void ordenaVetor(int *vetor);
+void bubbleSort(int *vetor, int tamanho);
 
 int main(void) {
     int lista[10] = {12, 42, 84, 22, 61, 9, 0, 11, 99, 35};
-    ordenaVetor(lista);
+    bubbleSort(lista, 10);
     return 0;
 }
 
-int buscaMenor(int *vetor) {
-    int menor = vetor[0], menor_indice = 0;
-
-    for(int k=0;k<10;k++) {
-        if(vetor[k] < menor) {
-            menor = vetor[k];
-            menor_indice = k;
+void bubbleSort(int *vetor, int tamanho) {
+    int temp, i, j;
+    for(i=0;i < tamanho;i++) { // loop externo indica quantas vezes se deve repetir o processo de varrer a array.
+        for(j=0;j < tamanho;j++) { // loop interno é para comparar 2 valores adjacentes e fazer a decisão da troca com o 'if'.
+            if(vetor[j] > vetor[j+1]) {
+                temp = vetor[j];
+                vetor[j] = vetor[j+1];
+                vetor[j+1] = temp;
+            }
         }
     }
-    return menor_indice;
-}
 
-void ordenaVetor(int *vetor) {
-    int novoVetor[10], menor;
-
-    printf("A sua nova Lista Ordenada é: ");
-    for(int k=0; k<10;k++) {
-        menor = buscaMenor(vetor);
-        novoVetor[k] = vetor[menor];
-    }
-
-    for(int k=0;k<10;k++) {
-        printf("%d ", novoVetor[k]);
+    printf("Seu vetor ficou assim: ");
+    for(i=0;i<tamanho;i++) {
+        printf("%d ", vetor[i]);
     }
 }
